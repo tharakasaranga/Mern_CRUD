6b2002c3ecd28245f96af23a06ab713197da903f
Tharaka Saranga <tharakasaranga755@gmail.com>
2025-04-02 00:14:06 +0530

    2. Finished Routing and API
    all CRUD codes are Successfully work

--- routes/posts.js
+++ routes/posts.js
@@ -29,4 +29,45 @@ router.get('/posts', async (req, res) => {
     }
 });
 
+
+//update post
+router.put('/post/update/:id', async (req, res) => {
+    try {
+        const updatedPost = await Posts.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+
+        if (!updatedPost) {
+            return res.status(404).json({ error: "Post not found" });
+        }
+
+        return res.status(200).json({
+            success: "Post Updated Successfully",
+            updatedPost
+        });
+    } catch (err) {
+        return res.status(400).json({ error: err.message });
+    }
+});
+
+
+
+//delete post
+router.delete('/post/delete/:id', async (req, res) => {
+    try {
+        const deletedPost = await Posts.findByIdAndDelete(req.params.id);
+
+        if (!deletedPost) {
+            return res.status(404).json({ error: "Post not found" });
+        }
+
+        return res.status(200).json({
+            message: "Post Deleted Successfully",
+            deletedPost
+        });
+    } catch (err) {
+        return res.status(400).json({ error: err.message });
+    }
+});
+
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+
+
 const port = 5000;
 const DB_URL = 'mongodb://localhost:27017/mernTutorial';
 
@@ -11,10 +13,10 @@ const postRoutes = require('./routes/posts');
 
 //import middleware
 app.use(bodyParser.json());
-
-
-
 app.use(postRoutes);
+
+
+
 mongoose.connect(DB_URL).then(()=>{
     console.log("DB Connected");
 }).catch((err) => {
