9d4f1f09dacd4c6e59d13dc213ee1c56593540de
Tharaka Saranga <tharakasaranga755@gmail.com>
2025-04-05 01:11:15 +0530

    rcc to rfc
    Convert Home js file from React Class component to React Functional Component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+const Home = () => {
+  const [posts, setPosts] = useState([]);
 
-export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { posts: [] };
-  }
+  useEffect(() => {
+    retrievePosts();
+  }, []);
 
-  componentDidMount() {
-    this.retrievePosts();
-  }
-
-  retrievePosts = () => {
+  const retrievePosts = () => {
     axios
       .get('http://localhost:5000/posts')
       .then((res) => {
         if (res.data.success) {
-          this.setState({ posts: res.data.existingPosts });
+          setPosts(res.data.existingPosts);
         }
       })
       .catch((error) => {
@@ -26,76 +22,73 @@ export default class Home extends Component {
       });
   };
 
-  handleDelete = (id) => {
+  const handleDelete = (id) => {
     if (window.confirm('Are you sure you want to delete this post?')) {
       axios
         .delete(`http://localhost:5000/post/delete/${id}`)
         .then(() => {
           alert('Post Deleted Successfully!');
-          this.retrievePosts(); // Refresh the posts list
+          retrievePosts(); // Refresh the posts list
         })
         .catch((error) => console.error('Error deleting post:', error));
     }
   };
 
-  render() {
-    return (
-      <div>
-        <div className="container mt-4">
-          <h1 className="mb-4 text-center">Posts</h1>
-          <div className="table-responsive">
-            <table className="table table-striped table-bordered">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Title</th>
-                  <th>Description</th>
-                  <th>Post Category</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.posts.map((post, index) => (
-                  <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>
+  return (
+    <div>
+      <div className="container mt-4">
+        <h1 className="mb-4 text-center">Posts</h1>
+        <div className="table-responsive">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Post Category</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.map((post, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <Link to={`/post/${post._id}`} className="text-decoration-none">
+                      {post.topic}
+                    </Link>
+                  </td>
+                  <td>{post.description}</td>
+                  <td>{post.postCategory}</td>
+                  <td>
+                    <div className="d-flex justify-content-center">
                       <Link
-                        to={`/post/${post._id}`}
-                        className="text-decoration-none"
+                        to={`/post/edit/${post._id}`}
+                        className="btn btn-warning btn-sm me-2"
                       >
-                        {post.topic}
+                        Edit
                       </Link>
-                    </td>
-                    <td>{post.description}</td>
-                    <td>{post.postCategory}</td>
-                    <td>
-                      <div className="d-flex justify-content-center">
-                        <Link
-                          to={`/post/edit/${post._id}`}
-                          className="btn btn-warning btn-sm me-2"
-                        >
-                          Edit
-                        </Link>
-                        <button
-                          onClick={() => this.handleDelete(post._id)}
-                          className="btn btn-danger btn-sm"
-                        >
-                          Delete
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <div className="text-center mt-3">
-            <Link to="/post/create" className="btn btn-primary">
-              Add New Post
-            </Link>
-          </div>
+                      <button
+                        onClick={() => handleDelete(post._id)}
+                        className="btn btn-danger btn-sm"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="text-center mt-3">
+          <Link to="/post/create" className="btn btn-primary">
+            Add New Post
+          </Link>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Home;
